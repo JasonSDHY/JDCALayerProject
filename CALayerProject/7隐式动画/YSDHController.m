882304e8d3bb5349e87b06a83017e3cb0f0dcf7f
Ding HongYu <447154278@qq.com>
2016-05-23 17:35:49 +0800

    创建隐式动画目录

---
+++ CALayerProject/7隐式动画/YSDHController.m
@@ -0,0 +1,37 @@
+//
+//  YSDHController.m
+//  CALayerProject
+//
+//  Created by Jason Ding on 16/5/23.
+//  Copyright © 2016年 Ding RedRain. All rights reserved.
+//
+
+#import "YSDHController.h"
+
+@interface YSDHController ()
+
+@end
+
+@implementation YSDHController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
